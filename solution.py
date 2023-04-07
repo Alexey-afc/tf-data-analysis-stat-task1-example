f0627f3701acb07a22458c6d2da5c2dc9359be3d
Alexey <55971755+Alexey-afc@users.noreply.github.com>
2023-04-07 20:32:04 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,4 +5,4 @@ import numpy as np
 chat_id = 543286418 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    return np.log(x-199).mean() # Ваш ответ
+    return (np.log(x-199)).mean() # Ваш ответ
