2281cd8f5fbb2cb016563571417cc41ad10a8e98
Alexey <55971755+Alexey-afc@users.noreply.github.com>
2023-04-07 17:52:10 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,4 +5,4 @@ import numpy as np
 chat_id = 543286418 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    return np.log(np.median(x-199)) # Ваш ответ
+    return np.log(np.median(x)-199) # Ваш ответ
